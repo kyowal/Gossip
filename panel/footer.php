d53181fe83539414a61ceb29cf3122c3bf1a465d
Rahul Siwal <rahulsiwal62@gmail.com>
2018-09-07 12:33:48 +0530

    Panel Files

---
+++ panel/footer.php
@@ -0,0 +1,8 @@
+</main>
+<footer >
+  <div class="container bg-info p-5">
+
+	</div>
+</footer>
+</body>
+</html>
---
+++ panel/header.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<link href="style.css" rel="stylesheet">
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+<header>
+  <div class="container bg-info p-5 ">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-item nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="#">Features</a>
+        </div>
+      </div>
+    </nav>
+  </div>
+</header>
+<!---->
+<main>
